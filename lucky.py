df8d5c0a14b167bf89c699880e0dcce1d3408bec
shaky <shaky>
2023-09-21 16:09:52 +0800

    “lucky”

---
+++ lucky.py
@@ -0,0 +1,10 @@
+
+lucky_dates = ['28 Jul 2020', '17 Jul 2021', '26 Dec 2021', '11 Jan 2022', '6 Jun 2022', '21 Jul 2022', '30 Nov 2022', '26 May 2023', '19 Sep 2023']
+
+n = int(input("Please enter the number of lucky dates you want to view: "))
+
+luckynumer = lucky_dates[-n:]
+print(luckynumer)
+
+
+# wangdongyang s235458
